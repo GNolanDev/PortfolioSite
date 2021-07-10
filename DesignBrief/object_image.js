f0a3d6ec12cac571588db1491260b5efd288a23e
gingergaz <gaz@cliffhanger.com>
2021-07-10 17:29:12 +0100

    Further styling research and wireframing - fonts, colours, etc

---
+++ DesignBrief/object_image.js
@@ -0,0 +1,20 @@
+const gareth = {
+  status: "aspiring full stack developer",
+  background: "eclectic",
+  skills: [
+    "html5",
+    "css3",
+    "js",
+    "php",
+    "sql",
+    "android(java & xml)",
+    "selenium testing",
+  ].concat(more),
+  areasOfInterest: ["education", "outdoor activities", "aviation"].concat(
+    lotsMore
+  ),
+  future: [
+    "building things that make the world a better place",
+    "learning more technologies",
+  ],
+};
