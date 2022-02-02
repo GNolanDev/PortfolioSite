b1eb7efabb07d18d424dadefb6ad3a5d4517fc81
GNolanDev <GNolanDev@gmail.com>
2022-02-02 18:56:55 +0000

    Add quick link to timerthing on projects page

--- public_html/projects.php
+++ public_html/projects.php
@@ -21,6 +21,14 @@
     ?>
     <main>
         <h1>Coming Soon...</h1>
+        <div>
+            Styling of this page, and links to other projects of various types to come in the not-too-distant future, in the meantime...
+        </div>
+        <div>My proudest project so far - <a href="www.timerthing.com">timerthing.com</a>.<br />
+            A live website built for amateur running (or other) groups to use based on QR codes, for timing their finishers week-in, week-out.<br />
+            Technologies used include html/css/js for front end, php back end utilising a MySQL database for storage of all user information, dates, times, events, locations etc. Email verification, password reset functionality, ability for multiple devices to time the same event simultaneously - lots of groovy stuff!<br />
+            Go get an account and try it out!
+        </div>
     </main>
     <?php
     include 'footer.php';
