986337b92a9e0a56bc212fee47cccb639c4404bd
GNolanDev <GNolanDev@gmail.com>
2022-02-09 12:29:20 +0000

    Add some styling to projects (more to do), add logos for java & xml, set site background to slightly off-white.

--- includes/projects-large.php
+++ includes/projects-large.php
@@ -1,14 +1,37 @@
 <section class="projects-page-content-section large-projects">
     <div class="project">
-        Some content here #1
+        <div class="project-title">Timerthing</div>
+        <div class="project-screenshot">
+            <img src="assets\images\hi-res\dummy-image.jpg" alt="">
+        </div>
+        <div class="project-description">
+            Timerthing is a public website I built from scratch to solve the problem of recording and storing times for a volunteer-led local running group following the closure of the national organisation which previously supported it. Users sign up and get a personal QR code, which event organisers can then scan in using an event timing page of the same application. Names are fetched from the server during the live event; multiple devices can be used for timing the same event; unknown finishers can be added, along with temporary names which users can 'claim' later; emergency contact info can optionally be added which can be made available to organisers or even just anyone with a smart phone since the QR code represents a real URL. Anyone with an account can set up their own location, with multiple events at each location, all of which can be run simultaneously in separate tabs. HTML, CSS, & Javascript is used for the front end & most of the actual timing functionality. JSON is used for communicating with the API created on the server side in PHP, with live events and long term storage, user account info etc all handled in MySQL (MariaDB during local development on XAMPP server). Hosted on Hostinger.com
+        </div>
+        <div class="project-link"><a href="https://www.timerthing.com">Link to timerthing.com</a></div>
+        <div class="project-technology-icons">
+            <div class="project-icon-html"></div>
+            <div class="project-icon-css"></div>
+            <div class="project-icon-js"></div>
+            <div class="project-icon-json"></div>
+            <div class="project-icon-php"></div>
+            <div class="project-icon-mariadb"></div>
+        </div>
+        <div class="project-open-iframe">Open in iFrame</div>
     </div>
     <div class="project">
-        Some content here #2
-    </div>
-    <div class="project">
-        Some content here #3
-    </div>
-    <div class="project">
-        Some content here #4
+        <div class="project-title">Sailor's Mate</div>
+        <div class="project-screenshot">
+            <img src="assets\images\hi-res\dummy-image.jpg" alt="">
+        </div>
+        <div class="project-description">
+            Sailor's Mate is an Android application I created whilst learning how to program using Java & XML in Android studio. It is a basic tool for finding a course to steer when sailing in todal waters in order to travel in the direction you mean to, accounting for the tide. It has day & night colour themes to protect night vision, and a further activity page which is a basic GPS position & speed readout, along with a 'remember this position' button which gives distance and direction to the point you've saved. This was the first large (for me!) project I attempted in coding, taking several months while also working full time, and was a valuable experience in wrestling with a user interface to make something clear and simple, yet functional too. It gave me chance to write automated tests in JUnit and Espresso, and go through the process of ensuring an app was secure and ready for production before deployment to the public with launch icons (home made!), screenshots etc.
+        </div>
+        <div class="project-link"><a href="https://play.google.com/store/apps/details?id=com.gingerphysics.sailorsmate">Link to Sailor's Mate (Google Play Store)<div class="project-inline-icon project-icon-playstore"></div></a>
+        </div>
+        <div class="project-technology-icons">
+            <div class="project-icon-android"></div>
+            <div class="project-icon-xml"></div>
+            <div class="project-icon-java"></div>
+        </div>
     </div>
 </section>
