f561d85fa3bb7787074b052fabfc2b0f3677a8f9
GNolanDev <GNolanDev@gmail.com>
2022-02-10 15:10:34 +0000

    UI: tweaks to small screen projects layout, addition of github logo.

--- includes/projects-little.php
+++ includes/projects-little.php
@@ -1,14 +1,23 @@
 <section class="projects-page-content-section little-projects">
     <div class="project">
-        Some content here #1
-    </div>
-    <div class="project">
-        Some content here #2
-    </div>
-    <div class="project">
-        Some content here #3
-    </div>
-    <div class="project">
-        Some content here #4
-    </div>
+        <div class="project-title">"Tea Cosy"</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_teacosy.png" alt="">
+        </div>
+        <div class="project-tldr">Code a simple webpage from a design brief</div>
+        <div class="project-description">
+            <label for="chkbx-1">
+                <input class="description-checkbox" type="checkbox" id="chkbx-1" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    Codecademy mini-project - follow a design brief, given image assets, to produce a webpage. Decided to make it responsive too.
+                </div>
+            </label>
+        </div>
+        <div class="project-link"><a href="https://gnolandev.github.io/teacosy/">gnolandev.github.io/teacosy</a></div>
+        <div class="project-github"><a href="https://github.com/GNolanDev/teacosy"></a></div>
+        <div class=" project-technology-icons">
+            <div class="project-icon-html"></div>
+            <div class="project-icon-css"></div>
+        </div>
 </section>
