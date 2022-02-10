29e413c77b16131fd5d4c226163fbd36f5077f1b
GNolanDev <GNolanDev@gmail.com>
2022-02-10 12:09:43 +0000

    large projects styling complete.

--- includes/projects-large.php
+++ includes/projects-large.php
@@ -2,14 +2,13 @@
     <div class="project">
         <div class="project-title">Timerthing</div>
         <div class="project-screenshot">
-            <img src="assets\images\hi-res\dummy-image.jpg" alt="">
+            <img src="assets/images/hi-res/sc_timerthing.png" alt="">
         </div>
         <div class="project-tldr">QR-based event timing website</div>
         <div class="project-description">
             <label for="chkbx-1">
                 <input class="description-checkbox" type="checkbox" id="chkbx-1" hidden>
-                <div class="project-description-inner moretext">description...
-                </div>
+                <div class="project-description-inner moretext"></div>
                 <div class="project-description-inner">
                     Timerthing is a public website I built from scratch to solve the problem of recording and storing times for a volunteer-led local running group following the closure of the national organisation which previously supported it. Users sign up and get a personal QR code, which event organisers can then scan in using an event timing page of the same application. Names are fetched from the server during the live event; multiple devices can be used for timing the same event; unknown finishers can be added, along with temporary names which users can 'claim' later; emergency contact info can optionally be added which can be made available to organisers or even just anyone with a smart phone since the QR code represents a real URL. Anyone with an account can set up their own location, with multiple events at each location, all of which can be run simultaneously in separate tabs. HTML, CSS, & Javascript is used for the front end & most of the actual timing functionality. JSON is used for communicating with the API created on the server side in PHP, with live events and long term storage, user account info etc all handled in MySQL (MariaDB during local development on XAMPP server). Hosted on Hostinger.com
                 </div>
@@ -28,20 +27,21 @@
     <div class="project">
         <div class="project-title">Sailor's Mate</div>
         <div class="project-screenshot">
-            <img src="assets\images\hi-res\dummy-image.jpg" alt="">
+            <img src="assets/images/hi-res/sc_sailorsmate.png" alt="">
         </div>
         <div class="project-tldr">Android app - does vector maths for sailors</div>
         <div class="project-description">
             <label for="chkbx-2">
                 <input class="description-checkbox" type="checkbox" id="chkbx-2" hidden>
-                <div class="project-description-inner moretext">description...
-                </div>
+                <div class="project-description-inner moretext"></div>
                 <div class="project-description-inner">
                     Sailor's Mate is an Android application I created whilst learning how to program using Java & XML in Android studio. It is a basic tool for finding a course to steer when sailing in todal waters in order to travel in the direction you mean to, accounting for the tide. It has day & night colour themes to protect night vision, and a further activity page which is a basic GPS position & speed readout, along with a 'remember this position' button which gives distance and direction to the point you've saved. This was the first large (for me!) project I attempted in coding, taking several months while also working full time, and was a valuable experience in wrestling with a user interface to make something clear and simple, yet functional too. It gave me chance to write automated tests in JUnit and Espresso, and go through the process of ensuring an app was secure and ready for production before deployment to the public with launch icons (home made!), screenshots etc.
                 </div>
             </label>
         </div>
-        <div class="project-link"><a href="https://play.google.com/store/apps/details?id=com.gingerphysics.sailorsmate">Sailor's Mate (Google Play Store)<div class="project-inline-icon project-icon-playstore"></div></a>
+        <div class="project-link android"><a href="https://play.google.com/store/apps/details?id=com.gingerphysics.sailorsmate">
+                <div class="project-inline-icon project-icon-playstore"></div>
+            </a>
         </div>
         <div class="project-technology-icons">
             <div class="project-icon-android"></div>
