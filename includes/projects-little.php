f27cdd17db8dad7d58001159172619d6b3f78ecb
GNolanDev <GNolanDev@gmail.com>
2022-02-11 13:29:02 +0000

    UX: some more little projects content added, styling for wider screens including slight enlarging on hover.

--- includes/projects-little.php
+++ includes/projects-little.php
@@ -1,6 +1,6 @@
 <section class="projects-page-content-section little-projects">
     <div class="project">
-        <div class="project-title">"Tea Cosy"</div>
+        <div class="project-title">Tea Cosy</div>
         <div class="project-screenshot">
             <img src="assets/images/hi-res/sc_teacosy.png" alt="">
         </div>
@@ -17,7 +17,53 @@
         <div class="project-link"><a href="https://gnolandev.github.io/teacosy/">gnolandev.github.io/teacosy</a></div>
         <div class="project-github"><a href="https://github.com/GNolanDev/teacosy"></a></div>
         <div class=" project-technology-icons">
-            <div class="project-tech-icon html" title="html"></div>
-            <div class="project-tech-icon css" title="css"></div>
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
         </div>
+    </div>
+    <div class="project">
+        <div class="project-title">Excursion</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_excursion.png" alt="">
+        </div>
+        <div class="project-tldr">Simple webpage from a design brief</div>
+        <div class="project-description">
+            <label for="chkbx-4-2">
+                <input class="description-checkbox" type="checkbox" id="chkbx-4-2" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    Another task to follow a design brief to produce a webpage. Again added further code to make it responsive.
+                </div>
+            </label>
+        </div>
+        <div class="project-link"><a href="https://gnolandev.github.io/excursion/">gnolandev.github.io/excursion</a></div>
+        <div class="project-github"><a href="https://github.com/GNolanDev/excursion"></a></div>
+        <div class=" project-technology-icons">
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
+        </div>
+    </div>
+    <div class="project">
+        <div class="project-title">Mixed Messages</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_euroballs.png" alt="">
+        </div>
+        <div class="project-tldr">Codecademy task - javascript random quote generator</div>
+        <div class="project-description">
+            <label for="chkbx-4-3">
+                <input class="description-checkbox" type="checkbox" id="chkbx-4-3" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    The challenge was to produce randomised quotes. I took this a bit further and collated funny commentator quotes from during and after games, and also the list of teams from Euro 2020, then had the code generate 2 random quotes & a random pairing of teams to display each time the button is clicked. Github link provided, but also hosted on netlify to try their Continuous deployment from Github system out (euroballs.netlify.app).
+                </div>
+            </label>
+        </div>
+        <div class="project-link"><a href="https://gnolandev.github.io/MixedMessages/">gnolandev.github.io/MixedMessages</a></div>
+        <div class="project-github"><a href="https://github.com/GNolanDev/MixedMessages"></a></div>
+        <div class=" project-technology-icons">
+            <div class="project-tech-icon js" title="Javascript"></div>
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
+        </div>
+    </div>
 </section>
