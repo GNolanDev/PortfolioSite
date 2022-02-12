b069bccf22fc6f93ac5917e23da0066a44d6ab50
GNolanDev <GNolanDev@gmail.com>
2022-02-12 18:41:34 +0000

    Most Offline project content added - todo: screenshot/video of socundi then 'about me' & contact page next.

--- includes/projects-offline.php
+++ includes/projects-offline.php
@@ -24,4 +24,32 @@
             <div class="project-tech-icon mariadb" title="MariaDB / MySQL"></div>
         </div>
     </div>
+    <div class="project">
+        <div class="project-title">Social Undistancing</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_baconbutty.png" alt="">
+        </div>
+        <div class="project-tldr">Connecting users with each other and tasks</div>
+        <div class="project-description">
+            <label for="chkbx-3-2">
+                <input class="description-checkbox" type="checkbox" id="chkbx-3-2" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    A challenging project - the idea was to create a website which would allow people ('helpees') to add 'tasks' (shopping, a lift to the doctor's etc), which anyone who was connected to them ('helpers') would be able to see, click to accept, and then update as they went along. Users would be able to have both roles and switch between them, and multiple helpers could be connected to one helpee, and could see if tasks had already been picked up by others.
+                    The challenge was to make the pages as accessible as possible, and I also set myself the goal of making everything work without any javascript at all in case a vulnerable person had it switched off for 'safety'.
+                    I accomplished this and added automated testing using Selenium (& Java). The database design was interesting, as I had to make use of lookup tables to connect users with each other and with tasks, and set up a secure hashed password storage & reset function, whilst keeping the database normalised. I also gave myself the challenge of displaying all times and dates in the users home country timezone, which involved having a country lookup table, storing all times in UTC & converting on the fly in PHP before sending to client machines.
+                    Much time was taken up learning how to wrangle CSS into an appealing state, adjusting by pixels here & there to fit icons and text into elements as clearly as possible.
+                    Once the main features of this project were working in my XAMPP development environment I then 'mothballed' it & moved on to a project which was needed to actually go live - <a href="projects.php#item-1">timerthing.com</a>.
+                </div>
+            </label>
+        </div>
+        <div class="project-link"></div>
+        <div class="project-github"></div>
+        <div class="project-technology-icons">
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon php" title="PHP"></div>
+            <div class="project-tech-icon mariadb" title="MariaDB / MySQL"></div>
+            <div class="project-tech-icon java" title="Java"></div>
+        </div>
+    </div>
 </section>
