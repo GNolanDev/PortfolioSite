0c88aae02b4f4b80a881f693ef3e14f659f83acc
GNolanDev <GNolanDev@gmail.com>
2022-02-12 17:58:26 +0000

    Add video to bacon butty finder description & style with border etc. Also style project titles to always clear the icons even if title is long enough to cause wrapping.

--- includes/projects-offline.php
+++ includes/projects-offline.php
@@ -8,19 +8,20 @@
         <div class="project-description">
             <label for="chkbx-3-1">
                 <input class="description-checkbox" type="checkbox" id="chkbx-3-1" hidden>
-                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner moretext">and video</div>
                 <div class="project-description-inner">
-                    A very early project, I wanted to learn how to embed maps and add layers and custom points and labels in my websites. After experimenting with Openlayers & Google Maps I found Leaflet to be a good mix of features, ease of use and lack of cost. Added OpenStreetMap for mapping data & experimented with the Geolocation API on my local XAMPP server. I combined this with some early work on providing secure account login & sessions to enable a user to dynamically add new locatons by clicking on the map. These would then be added to an SQL database for retrieval and display on the map. I had fun creating a custom png icon & shadow too, finding out a lot about vector/scalar graphics and how to use editors in the process. Once I had learned enough, I mothballed the project at the early prototype stage on my local development machine & moved on to my 'social undistancing' app.
+                    A very early project, I wanted to learn how to embed maps and add layers and custom points and labels in my websites. After experimenting with Openlayers & Google Maps I found Leaflet to be a good mix of features, ease of use and lack of cost. Added OpenStreetMap for mapping data & experimented with the Geolocation API on my local XAMPP server. I combined this with some early work on providing secure account login & sessions to enable a user to dynamically add new locations by clicking on the map. These would then be added to an SQL database for retrieval and display on the map. I had fun creating a custom 'bacon butty' png icon & shadow too, finding out a lot about vector/scalar graphics and how to use editors in the process. Once I had learned enough, I mothballed the project at the early prototype stage on my local development machine & moved on to my 'social undistancing' app.
+                    <video src="assets/videos/scv_baconbuttyfinder.mp4" autoplay muted loop controls>Quiz app screen capture video</video>
                 </div>
             </label>
         </div>
         <div class="project-link"></div>
-        <div class="project-github"><a href="https://github.com/GNolanDev/PortfolioSite" target="_blank"></a></div>
+        <div class="project-github"></div>
         <div class="project-technology-icons">
             <div class="project-tech-icon html" title="HTML"></div>
-            <div class="project-tech-icon css" title="CSS"></div>
             <div class="project-tech-icon js" title="Javascript"></div>
             <div class="project-tech-icon php" title="PHP"></div>
+            <div class="project-tech-icon mariadb" title="MariaDB / MySQL"></div>
         </div>
     </div>
 </section>
