284d314a8979b716abfe98ce83246f15451044e9
GNolanDev <GNolanDev@gmail.com>
2022-02-12 10:53:20 +0000

    UX: Add more to other projects section, resize some screenshots to reduce file size, add react icon.

--- includes/projects-other.php
+++ includes/projects-other.php
@@ -7,21 +7,45 @@
         <div class="project-tldr">My portfolio website</div>
         <div class="project-description">
             <label for="chkbx-2-1">
-                <input class="description-checkbox" type="checkbox" id="chkbx-1" hidden>
+                <input class="description-checkbox" type="checkbox" id="chkbx-2-1" hidden>
                 <div class="project-description-inner moretext"></div>
                 <div class="project-description-inner">
                     My portfolio website. Originally begun as a small task during the Codecademy Full Stack Developer course, I have added further styling and content to showcase more of my work & experiment with ways to add styling to webpages.
-                    My main priority was to keep the site accessible and responsive to different screen sizes - this is an ongoing process, so more sections will be completed and added to over time. The codebase for the site is available on github using the link below.
+                    My main priority was to keep the site accessible and responsive to different screen sizes while trying out various css 'tricks' etc - this is an ongoing process, so more sections will be completed and added to over time. The codebase for the site is available on github using the link below.
                 </div>
             </label>
         </div>
         <div class="project-link"></div>
         <div class="project-github"><a href="https://github.com/GNolanDev/PortfolioSite" target="_blank"></a></div>
         <div class="project-technology-icons">
-            <div class="project-tech-icon html"></div>
-            <div class="project-tech-icon css"></div>
-            <div class="project-tech-icon js"></div>
-            <div class="project-tech-icon php"></div>
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
+            <div class="project-tech-icon js" title="Javascript"></div>
+            <div class="project-tech-icon php" title="PHP"></div>
+        </div>
+    </div>
+    <div class="project">
+        <div class="project-title">Jammming</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_jammming.png" alt="">
+        </div>
+        <div class="project-tldr">React app - Spotify API</div>
+        <div class="project-description">
+            <label for="chkbx-2-2">
+                <input class="description-checkbox" type="checkbox" id="chkbx-2-2" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    A project set by Codecademy as part of the full stack path. Uses Create-React-App as a base, html templates given. My job was to create all the components and connect them up, then connect to the Spotify API to fetch songs & create new playlists on the user's account. This was a good overview of how React single page apps are arranged, a chance to see how API documentation works, an opportunity to use build tools, and an introduction to a simple way of deploying via 'surge'.
+                </div>
+            </label>
+        </div>
+        <div class="project-link"><a href="https://gnolandevjammming.netlify.app" target="_blank"></a></div>
+        <div class="project-github"><a href="https://github.com/GNolanDev/jammming" target="_blank"></a></div>
+        <div class="project-technology-icons">
+            <div class="project-tech-icon react" title="React"></div>
+            <div class="project-tech-icon js" title="Javascript"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
+            <div class="project-tech-icon html" title="HTML"></div>
         </div>
     </div>
 </section>
