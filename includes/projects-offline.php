bb652270cc5d12c4a3fcf7ecc0933be3a472bc5f
GNolanDev <GNolanDev@gmail.com>
2022-02-12 17:01:10 +0000

    Add content to offline projects page, restyle screenshot grid area a little.

--- includes/projects-offline.php
+++ includes/projects-offline.php
@@ -1,14 +1,26 @@
 <section class="projects-page-content-section offline-projects">
     <div class="project">
-        Some content here #1
-    </div>
-    <div class="project">
-        Some content here #2
-    </div>
-    <div class="project">
-        Some content here #3
-    </div>
-    <div class="project">
-        Some content here #4
+        <div class="project-title">Bacon Butty Finder</div>
+        <div class="project-screenshot">
+            <img src="assets/images/hi-res/sc_baconbutty.png" alt="">
+        </div>
+        <div class="project-tldr">Learning project - playing with geospatial data using Leaflet.js</div>
+        <div class="project-description">
+            <label for="chkbx-3-1">
+                <input class="description-checkbox" type="checkbox" id="chkbx-3-1" hidden>
+                <div class="project-description-inner moretext"></div>
+                <div class="project-description-inner">
+                    A very early project, I wanted to learn how to embed maps and add layers and custom points and labels in my websites. After experimenting with Openlayers & Google Maps I found Leaflet to be a good mix of features, ease of use and lack of cost. Added OpenStreetMap for mapping data & experimented with the Geolocation API on my local XAMPP server. I combined this with some early work on providing secure account login & sessions to enable a user to dynamically add new locatons by clicking on the map. These would then be added to an SQL database for retrieval and display on the map. I had fun creating a custom png icon & shadow too, finding out a lot about vector/scalar graphics and how to use editors in the process. Once I had learned enough, I mothballed the project at the early prototype stage on my local development machine & moved on to my 'social undistancing' app.
+                </div>
+            </label>
+        </div>
+        <div class="project-link"></div>
+        <div class="project-github"><a href="https://github.com/GNolanDev/PortfolioSite" target="_blank"></a></div>
+        <div class="project-technology-icons">
+            <div class="project-tech-icon html" title="HTML"></div>
+            <div class="project-tech-icon css" title="CSS"></div>
+            <div class="project-tech-icon js" title="Javascript"></div>
+            <div class="project-tech-icon php" title="PHP"></div>
+        </div>
     </div>
 </section>
