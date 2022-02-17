cdbbd8460c04dcfb7d25c40e7cade3002f82a278
GNolanDev <GNolanDev@gmail.com>
2022-02-17 01:48:42 +0000

    Style footer icons for light and dark themes.

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,21 @@
 <!-- footer for all pages on portfolio site -->
 <footer id="pageFooter">
     <section id="socialContainer">
-        <div id="logoDecorationContainer">
-            <div class="logo" id="logoDecoration"></div>
-        </div>
-        <div class="icon-holder">
+        <div class="icon-holder-footer">
             <a href="mailto:GNolanDev@gmail.com" title="email me">
                 <div id="email-icon" class="icon">
-                    <img src="assets/icons/ic_mail.svg" alt="" title="email me" />
                 </div>
             </a>
         </div>
-        <div class="icon-holder">
+        <div class="icon-holder-footer">
             <a href="https://m.me/gareth.j.nolan" target="_blank" title="facebook messenger">
                 <div id="fb-icon" class="icon">
-                    <img src="assets/icons/ic_facebook_dark.svg" alt="" title="email me" />
                 </div>
             </a>
         </div>
-        <div class="icon-holder">
+        <div class="icon-holder-footer">
             <a href="https://www.linkedin.com/in/gareth-nolan-web" target="_blank" title="Linkedin">
                 <div id="linkedin-icon" class="icon">
-                    <img src="assets/icons/ic_linkedin.svg" alt="" title="email me" />
                 </div>
             </a>
         </div>
