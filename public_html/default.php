55c71834128d52f21647347e3c5961caa2added2
GNolanDev <GNolanDev@gmail.com>
2022-02-18 17:37:58 +0000

    Add default.php to production branch for easy maintenance on server.

---
+++ public_html/default.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" class="index-page-root">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Gareth Nolan's homepage" />
+    <title>GNolan Developer Portfolio</title>
+</head>
+
+<body>
+    <main>
+        <h1 style="text-align: center; padding: 12px">Gareth's portfolio site - down for maintenance for a while, back soon!</h1>
+    </main>
+</body>
+
+</html>
